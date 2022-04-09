b6ae4566c7c52d5904ab561f68801d8e1770e581
danylo.sydorchuk <danylo.sydorchuk@PK6-07>
2022-04-09 14:23:48 +0200

    Dodaj pliki projektów.

---
+++ Zad1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var FileUrl = @"D:\Users\danylo.sydorchuk\Documents\kod.txt";
+
+            //file lines
+            string[] lines = File.ReadAllLines(FileUrl);
+
+            //loop through each file line
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
---
+++ Zad2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"D:\Users\danylo.sydorchuk\Documents\kodnowy.txt";
+
+            // Delete the file if it exists.
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+
+            using (FileStream fs = File.Create(path))
+            {
+                Console.WriteLine();
+                AddText(fs, Console.ReadLine());
+                for (int i = 1; i < 120; i++)
+                {
+                    
+                    AddText(fs, Convert.ToChar(i).ToString());
+                }
+            }
+
+            //Open the stream and read it back.
+            using (FileStream fs = File.OpenRead(path))
+            {
+                byte[] b = new byte[1024];
+                UTF8Encoding temp = new UTF8Encoding(true);
+                while (fs.Read(b, 0, b.Length) > 0)
+                {
+                    Console.WriteLine(temp.GetString(b));
+                }
+            }
+        }
+
+        private static void AddText(FileStream fs, string value)
+        {
+            byte[] info = new UTF8Encoding(true).GetBytes(value);
+            fs.Write(info, 0, info.Length);
+        }
+    }
+}
